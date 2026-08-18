df9d06a0468af3ea35fdc8466ec39f6d62d484c8
int16 <int16int8@gmail.com>
2026-08-18 20:20:19 +0800

    feat: add caddy to nix shell

--- shell.nix
+++ shell.nix
@@ -2,10 +2,11 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   name = "nodejs22";
-  
+
   buildInputs = with pkgs; [
     nodejs_22
     npm-check-updates
+    caddy
   ];
   shellHook = '''';
 }
